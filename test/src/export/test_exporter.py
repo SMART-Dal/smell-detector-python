48af21e072fe40a0147633debd00b2ddd8889fe9
akaVagh <harshvaghani00@gmail.com>
2023-12-25 15:59:45 -0400

    Complete tests for implementation smell and metrics; update README - Finalized all tests related to implementation smell and metrics - Total test cases written: 145 - Updated README with relevant information and instructions

--- test/src/export/test_exporter.py
+++ test/src/export/test_exporter.py
@@ -6,19 +6,19 @@ from src.export.exporter import export_to_json, export_to_csv
 
 # Sample data for testing
 sample_data = [{
-        "package": "input_package",
-        "module_name": "test_module.py",
-        "class_name": "TestClass",
-        "loc": 13,
-        "wmc": 3,
-        "nom": 3,
-        "nopm": 2,
-        "nof": 0,
-        "nopf": 0,
-        "lcom": 2,
-        "fan_in": 3,
-        "fan_out": 4
-    }
+    "package": "input_package",
+    "module_name": "test_module.py",
+    "class_name": "TestClass",
+    "loc": 13,
+    "wmc": 3,
+    "nom": 3,
+    "nopm": 2,
+    "nof": 0,
+    "nopf": 0,
+    "lcom": 2,
+    "fan_in": 3,
+    "fan_out": 4
+}
 ]
 
 # Test directory and file names
@@ -57,16 +57,18 @@ def create_test_output_dir():
 
 # Test successful JSON export
 def test_export_to_json_success(mock_open, mock_json_dump, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.json")
     export_to_json(sample_data, test_output_dir, test_file_name)
     mock_json_dump.assert_called_once()
-    logging.info.assert_called_with(f"JSON data successfully exported to {test_output_dir}\\{test_file_name}.json")
+    logging.info.assert_called_with(f"JSON data successfully exported to {expected_path}")
 
 
 # Test successful CSV export
 def test_export_to_csv_success(mock_open, mock_csv_writer, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.csv")
     export_to_csv(sample_data, test_output_dir, test_file_name)
     mock_csv_writer.assert_called_once()
-    logging.info.assert_called_with(f"CSV data successfully exported to {test_output_dir}\\{test_file_name}.csv")
+    logging.info.assert_called_with(f"CSV data successfully exported to {expected_path}")
 
 
 # Test handling of empty data for CSV
@@ -77,31 +79,35 @@ def test_export_to_csv_no_data(mock_logging):
 
 # Test IOError handling for JSON
 def test_export_to_json_io_error(mock_open, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.json")
     mock_open.side_effect = IOError("Test IOError")
     export_to_json(sample_data, test_output_dir, test_file_name)
     logging.error.assert_called_with(
-        f"IOError while writing JSON to {test_output_dir}\\{test_file_name}.json: Test IOError")
+        f"IOError while writing JSON to {expected_path}: Test IOError")
 
 
 # Test IOError handling for CSV
 def test_export_to_csv_io_error(mock_open, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.csv")
     mock_open.side_effect = IOError("Test IOError")
     export_to_csv(sample_data, test_output_dir, test_file_name)
     logging.error.assert_called_with(
-        f"IOError while writing CSV to {test_output_dir}\\{test_file_name}.csv: Test IOError")
+        f"IOError while writing CSV to {expected_path}: Test IOError")
 
 
 # Test unexpected error handling for JSON
 def test_export_to_json_unexpected_error(mock_open, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.json")
     mock_open.side_effect = Exception("Unexpected Error")
     export_to_json(sample_data, test_output_dir, test_file_name)
     logging.error.assert_called_with(
-        f"Unexpected error while writing JSON to {test_output_dir}\\{test_file_name}.json: Unexpected Error")
+        f"Unexpected error while writing JSON to {expected_path}: Unexpected Error")
 
 
 # Test unexpected error handling for CSV
 def test_export_to_csv_unexpected_error(mock_open, mock_logging, create_test_output_dir):
+    expected_path = os.path.join(test_output_dir, f"{test_file_name}.csv")
     mock_open.side_effect = Exception("Unexpected Error")
     export_to_csv(sample_data, test_output_dir, test_file_name)
     logging.error.assert_called_with(
-        f"Unexpected error while writing CSV to {test_output_dir}\\{test_file_name}.csv: Unexpected Error")
+        f"Unexpected error while writing CSV to {expected_path}: Unexpected Error")
