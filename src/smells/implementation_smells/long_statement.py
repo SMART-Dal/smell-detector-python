acfa608776ab866cf5215bb9b7c014f98b96c839
Dhruv Upadhyay <dhupadhyay2000@gmail.com>
2024-03-20 17:53:24 -0300

    Edge case for looping and conditional statements added to detect long statement

--- src/smells/implementation_smells/long_statement.py
+++ src/smells/implementation_smells/long_statement.py
@@ -17,18 +17,26 @@ class LongStatementDetector(ImplementationSmellDetector):
     def _detect_smells(self, module, config):
         logging.info(f"Starting long statement detection in module: {module.name}")
         smells = []
-        max_length = config.get("threshold", 100)  # Default to 100 if not specified
+        max_length = config.get("threshold", 100)
 
         for entity in self._iterate_functions_and_methods(module):
             try:
                 for statement in entity.function_body:
                     statement_str = _ast_to_string(statement)
-                    print(statement_str)
-                    if len(statement_str) > max_length:
-                        detail = f"A statement with {len(statement_str)} chars in {entity.name} exceeds the maximum length of {max_length} characters."
-                        smells.append(self._create_smell(module.name, entity, detail, statement.lineno))
-                        logging.info(
-                            f"Long statement ({len(statement_str)} chars) detected in {entity.name} at line {statement.lineno} in {module.name}")
+                    if isinstance(statement, (ast.For, ast.While, ast.If)):
+                        # For loop, while loop, or if statement found
+                        for line_number, line in enumerate(statement_str.split('\n'), start=statement.lineno):
+                            if len(line.strip()) > max_length:
+                                detail = f"A line with {len(line.strip())} chars in {entity.name} exceeds the maximum length of {max_length} characters."
+                                smells.append(self._create_smell(module.name, entity, detail, line_number))
+                                logging.info(
+                                    f"Long line ({len(line.strip())} chars) detected in {entity.name} at line {line_number} in {module.name}")
+                    else:
+                        if len(statement_str) > max_length:
+                            detail = f"A statement with {len(statement_str)} chars in {entity.name} exceeds the maximum length of {max_length} characters."
+                            smells.append(self._create_smell(module.name, entity, detail, statement.lineno))
+                            logging.info(
+                                f"Long statement ({len(statement_str)} chars) detected in {entity.name} at line {statement.lineno} in {module.name}")
             except Exception as e:
                 logging.error(f"Error analyzing {entity.name} in {module.name}: {e}", exc_info=True)
 
